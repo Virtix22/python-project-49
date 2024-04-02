01d0d69dd9200c7dbdaaafd2ef56fe61ac464b15
Victor Voronov <virtix22@gmail.com>
2024-04-02 23:28:42 +0300

    add cli.py script.py modified

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
+from brain_games import cli
+
 def greetings():
     print('Welcome to the Brain Games!')
 
 
 def main():
     greetings()
+    cli.welcome_user()
 
-    
 if __name__ == '__main__':
     main()
