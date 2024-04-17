d5fec98cc240a8a145d908217b6e9637e265435e
Victor Voronov <virtix22@gmail.com>
2024-04-17 07:35:07 +0300

    change name of right result in prime_body

--- brain_games/games/brain_prime_body.py
+++ brain_games/games/brain_prime_body.py
@@ -6,5 +6,5 @@ from brain_games.games.engine.functions import prime, random_num
 def brain_prime():
     rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
     rand_num = random_num()
-    result = prime(rand_num)
-    return rand_num, result, rules
+    rigt_result = prime(rand_num)
+    return rand_num, rigt_result, rules
